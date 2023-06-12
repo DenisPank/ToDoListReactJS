a6cf26ebc1ec7a6ec10854ac4498e5e1c1498e80
DenisPank <den3000d.1267@gmail.com>
2023-06-12 14:41:50 +0300

    start project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react'
-
-import { LogIn } from './pages/Login'
+import { ToDoList } from 'ToDoList'
 
 const App: FC = () => {
+  const tasks1 = [
+    { id: 1, title: 'css', isDone: true },
+    { id: 2, title: 'JS', isDone: true },
+    { id: 3, title: 'React', isDone: false }
+  ]
+  const tasks2 = [
+    { id: 1, title: 'Term', isDone: true },
+    { id: 2, title: 'XXX', isDone: false },
+    { id: 3, title: 'Jentlmens', isDone: false }
+  ]
+
   return (
     <div className="App">
-      <LogIn />
+      <ToDoList title="what tp learn" tasks={tasks1} />
+      <ToDoList title="Movies" tasks={tasks2} />
     </div>
   )
 }
